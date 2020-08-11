1525aad1a274d82ceb70211a3cf03d475de64e7a
SunriseBM <58320732+SunriseBM@users.noreply.github.com>
2020-08-11 07:36:56 -0700

    upload file functions KT_format_money
    upload file functions KT_format_money

--- models/functions.js
+++ models/functions.js
@@ -1,6 +1,5 @@
 //=========[BEGIN: Format tiền]=========//
-function format_money(value)
-{
+function format_money(value){
  let val = (value/1).toFixed(0).replace('.', ',')
  return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
 }
