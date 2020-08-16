c12a5c690fe088b134a14a7d5da9e30890a97a01
nguyenthitamnhu <58318374+nguyenthitamnhu@users.noreply.github.com>
2020-08-16 21:13:48 +0700

    Update outlogin.js

--- middlewares/khachhang/outlogin.js
+++ middlewares/khachhang/outlogin.js
@@ -1,8 +1,12 @@
 module.exports = function outloginKH(req, res, next) {
 	//kiem tra trang ko su dung cookie
     if (req.kwKH ) {
+
         res.redirect('/dangxuat');
+
     } else {
+
         return next();
+
     }
 }
